7d68ed82201834f6805062bccd73209395b82d43
Gunavant-07 <gunavantdesai07@gmail.com>
2026-03-11 14:25:39 +0530

    upadate changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,21 @@
 // Gujarat Smart Recipe Recommendation System - JavaScript
 
-// Firebase Setup for Auth
+// Firebase Setup for Auth'
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from 'https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js';
 
 // Your web app's Firebase configuration
 
-const firebaseConfig = window.firebaseConfig;
+const firebaseConfig = {
+  apiKey: "AIzaSyDCJsc1P1swgnDvqlejcjo9uq60BdxHmBI",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
